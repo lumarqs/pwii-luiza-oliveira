55aa0eb20b0a5dc3209f7c716d73d6548c409073
Luiza Marques <analuiza.05marquesoli@gmail.com>
2025-05-06 16:37:38 -0300

    Exercícios de index

---
+++ aula05/index.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+
+        <form action="mensagem.php" method="POST">
+            Digite sua mensagem:
+            <br>
+            <input type="text" name="msg" id="msg"><br>
+            <input type="text" name="nome" id="nome"><br>
+            <input type="submit">
+        </form>
+    </body>
+</html>
---
+++ aula05/mensagem.php
@@ -0,0 +1,6 @@
+<html>
+    <body>
+        <h1><?php echo $_POST["msg"]; ?></h1>
+        <h1><?php echo $_POST["nome"]; ?></h1>
+</body>
+</html> 
